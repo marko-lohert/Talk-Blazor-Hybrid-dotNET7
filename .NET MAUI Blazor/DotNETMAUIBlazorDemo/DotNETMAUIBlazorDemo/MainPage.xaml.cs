457933d0d9f8e0585977372f0e712e9bdcb4b1a3
Marko Lohert <lohert.marko@gmail.com>
2022-11-27 16:38:47 +0100

    .NET MAUI Blazor app created from a template in Visual Studio (template name is ".NET MAUI Blazor App").

---
+++ .NET MAUI Blazor/DotNETMAUIBlazorDemo/DotNETMAUIBlazorDemo/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNETMAUIBlazorDemo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ .NET MAUI Blazor/DotNETMAUIBlazorDemo/DotNETMAUIBlazorDemo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DotNETMAUIBlazorDemo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
